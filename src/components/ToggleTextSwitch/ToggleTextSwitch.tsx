34b6bf30c2c3cdff4e4685fb0a3eee18ceff8870
junyeop <junyeop.kim@snplab.io>
2022-08-08 17:25:50 +0900

    fix: toggle-text-switch name 설정

--- src/components/ToggleTextSwitch/ToggleTextSwitch.tsx
+++ src/components/ToggleTextSwitch/ToggleTextSwitch.tsx
@@ -1,15 +1,19 @@
-import { useState } from 'react'
+import { ChangeEventHandler, useState } from 'react'
 import styles from './toggleTextSwitch.module.scss'
 
 interface IProps {
+  name: string
   dataList: { id: string; text: string }[]
 }
 
-export default function ToggleTextSwitch({ dataList }: IProps) {
+export default function ToggleTextSwitch({ name, dataList }: IProps) {
   const [currentId, setCurrentId] = useState(0)
-  const handleClick = (id: number) => () => setCurrentId(id)
-
   const size = dataList.length
+
+  const handleClick: ChangeEventHandler<HTMLInputElement> = (e) => {
+    const { value } = e.target
+    setCurrentId(Number(value))
+  }
 
   const sliderStyle = (id: number) => ({
     '--size': `calc(100% / ${size})`,
@@ -23,14 +27,14 @@ export default function ToggleTextSwitch({ dataList }: IProps) {
         {dataList.map((data, index) => (
           <div className={styles.inputBox} key={data.id}>
             <input
-              type='radio'
-              id={data.id}
-              name='switch'
+              type='checkbox'
+              id={`${name}-${data.id}`}
+              name={name}
               value={index}
-              onChange={handleClick(index)}
+              onChange={handleClick}
               checked={currentId === index}
             />
-            <label htmlFor={data.id}>{data.text}</label>
+            <label htmlFor={`${name}-${data.id}`}>{data.text}</label>
           </div>
         ))}
 
